73ad0e0c9088ccb03e3d2b9744c0c83a0ce2cf9e
ivponicka <56002673+ivponicka@users.noreply.github.com>
2021-08-03 15:56:03 +0200

    Add files via upload

---
+++ Unit1.h
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TShape *background;
+        TImage *ball;
+        TTimer *timerball;
+        TImage *paddle;
+        TTimer *left;
+        TTimer *right;
+        TImage *Image10;
+        TImage *Image1;
+        TImage *Image2;
+        TImage *Image3;
+        TImage *Image4;
+        TImage *Image5;
+        TImage *Image6;
+        TImage *Image7;
+        TImage *Image8;
+        TButton *Button1;
+        TImage *Image9;
+        void __fastcall timerballTimer(TObject *Sender);
+        void __fastcall leftTimer(TObject *Sender);
+        void __fastcall rightTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
